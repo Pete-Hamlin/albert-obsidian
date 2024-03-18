86dec46d86bcb365ca7e9da45e6834f1f103ba7f
pete <work@pete-hamlin.com>
2024-03-18 13:29:51 +0000

    feat: Overhaul extension to use a IndexQuery and filesystem watcher thread

--- __init__.py
+++ __init__.py
@@ -7,18 +7,21 @@ Searches notes in an Obsidian vault. Allows for creation of new notes in the sel
 
 from pathlib import Path
 from urllib import parse
+from threading import Thread, Event
+from watchfiles import Change, DefaultFilter, watch
 
 import frontmatter
 from albert import *
 
-md_iid = "2.1"
-md_version = "0.5"
+md_iid = "2.2"
+md_version = "1.0"
 md_name = "Obsidian"
 md_id = "obsidian"
 md_description = "Search/add notes in a Obsidian vault."
+md_url = "https://github.com/Pete-Hamlin/albert-obsidian.git"
 md_license = "MIT"
-md_maintainers = ["@Pete-Hamlin"]
-md_lib_dependencies = ["python-frontmatter"]
+md_authors = ["@Pete-Hamlin"]
+md_lib_dependencies = ["python-frontmatter", "watchfiles"]
 
 
 class Note:
@@ -26,15 +29,42 @@ class Note:
         self.path = path
         self.body = body
 
+class CDFilter(DefaultFilter):
+    """
+    When it comes to indexing, we don't care about updates to the file, so this filter
+    allows us to just watch for create/delete events and re-index accordingly when they happen
+    """
+    allowed_changes = [Change.added, Change.deleted]
 
-class Plugin(PluginInstance, GlobalQueryHandler):
+    def __call__(self, change: Change, path: str) -> bool:
+        return change in self.allowed_changes and super().__call__(change, path)
+
+class FileWatcherThread(Thread):
+    def __init__(self, callback, path, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.__stop_event = Event()
+        self.__callback = callback
+        self.__path = path
+
+    def run(self):
+        # Watch for file changes and re-index
+        for _ in watch(self.__path, watch_filter=CDFilter(), stop_event=self.__stop_event):
+            self.__callback()
+
+    def stop(self):
+        self.__stop_event.set()
+
+
+class Plugin(PluginInstance, IndexQueryHandler):
     iconUrls = [
         f"file:{Path(__file__).parent}/obsidian.png",
         "xdg:folder-documents",
     ]
 
     def __init__(self):
-        GlobalQueryHandler.__init__(self, id=md_id, name=md_name, description=md_description, defaultTrigger="obs ")
+        IndexQueryHandler.__init__(
+            self, id=md_id, name=md_name, description=md_description, defaultTrigger="obs ", synopsis="<note>"
+        )
         PluginInstance.__init__(self, extensions=[self])
 
         self._root_dir = self.readConfig("root_dir", str) or ""
@@ -44,6 +74,12 @@ class Plugin(PluginInstance, GlobalQueryHandler):
         self._filter_by_body = self.readConfig("filter_by_body", bool) or False
 
         self.root_path = Path(self._root_dir)
+        self.thread = FileWatcherThread(self.updateIndexItems, self._root_dir)
+        self.thread.start()
+
+    def finalize(self):
+        self.thread.stop()
+        self.thread.join()
 
     @property
     def root_dir(self):
@@ -53,7 +89,12 @@ class Plugin(PluginInstance, GlobalQueryHandler):
     def root_dir(self, value):
         self._root_dir = value
         self.writeConfig("root_dir", value)
+        
         self.root_path = Path(value)
+        self.thread.stop()
+        self.thread.join()
+        self.thread = FileWatcherThread(self.updateIndexItems, self._root_dir)
+        self.thread.start()
 
     @property
     def open_override(self):
@@ -63,7 +104,6 @@ class Plugin(PluginInstance, GlobalQueryHandler):
     def open_override(self, value):
         self._open_override = value
         self.writeConfig("open_override", value)
-        self.root_path = Path(value)
 
     @property
     def filter_by_tags(self):
@@ -91,14 +131,26 @@ class Plugin(PluginInstance, GlobalQueryHandler):
             {"type": "checkbox", "property": "filter_by_body", "label": "Filter by note body"},
         ]
 
+    def updateIndexItems(self):
+        notes = self.parse_notes()
+        index_items = []
+
+        for note in notes:
+            filter = self.create_filters(note)
+            item = self.gen_item(note)
+            index_items.append(IndexItem(item=item, string=filter))
+        self.setIndexItems(index_items)
+        info("Indexed {} notes".format(len(index_items)))
+
     def handleTriggerQuery(self, query):
+        # Trigger query will ignore the index and always check against the latest vault state
         stripped = query.string.strip()
         if stripped:
             if not query.isValid:
                 return
             data = self.parse_notes()
             notes = (item for item in data if stripped.lower() in self.create_filters(item))
-            items = [item for item in self.gen_items(notes)]
+            items = [self.gen_item(item) for item in notes]
             text = parse.urlencode({"vault": self.root_path.name, "name": stripped}, quote_via=parse.quote)
             run_args = self._open_override.split() + [f"obsidian://new?{text}"]
             query.add(items)
@@ -122,22 +174,12 @@ class Plugin(PluginInstance, GlobalQueryHandler):
                 StandardItem(id=md_id, text=md_name, subtext="Search for a note in Obsidian", iconUrls=self.iconUrls)
             )
 
-    def handleGlobalQuery(self, query):
-        stripped = query.string.strip()
-        if stripped:
-            if not query.isValid:
-                return
-            data = self.parse_notes()
-            notes = (item for item in data if stripped.lower() in self.create_filters(item))
-            items = [RankItem(item=item, score=0) for item in self.gen_items(notes)]
-            return items
-
     def parse_notes(self):
         for item in self.root_path.rglob("*.md"):
             yield Note(item, frontmatter.load(item))
 
-    def create_filters(self, note: Note):
-        filters, tags = str(note.path), note.body.get("tags")
+    def create_filters(self, note: Note) -> str:
+        filters, tags = note.path.name, note.body.get("tags")
         if self._filter_by_tags and tags:
             if isinstance(tags, list):
                 filters += ",".join(tags)
@@ -147,26 +189,25 @@ class Plugin(PluginInstance, GlobalQueryHandler):
             filters += note.body.content
         return filters.lower()
 
-    def gen_items(self, notes: list[Note]):
-        for note in notes:
-            tags = note.body.get("tags")
-            if tags:
-                subtext = " - ".join([str(note.path), ",".join(tags)])
-            else:
-                subtext = str(note.path)
-            note_uri = parse.urlencode({"vault": self.root_path.name, "file": note.path.name}, quote_via=parse.quote)
-            run_args = self._open_override.split() + [f"obsidian://open?{note_uri}"]
-            yield StandardItem(
-                id=md_id,
-                text=note.path.name.replace(".md", ""),
-                subtext=subtext,
-                iconUrls=self.iconUrls,
-                actions=[
-                    Action(
-                        "open",
-                        "Open",
-                        lambda args=run_args: runDetachedProcess(args),
-                    ),
-                    Action("copy", "Copy URI", lambda uri=note_uri: setClipboardText(uri)),
-                ],
-            )
+    def gen_item(self, note: Note):
+        tags = note.body.get("tags")
+        if tags:
+            subtext = " - ".join([str(note.path), ",".join(tags)])
+        else:
+            subtext = str(note.path)
+        note_uri = parse.urlencode({"vault": self.root_path.name, "file": note.path.name}, quote_via=parse.quote)
+        run_args = self._open_override.split() + [f"obsidian://open?{note_uri}"]
+        return StandardItem(
+            id=md_id,
+            text=note.path.name.replace(".md", ""),
+            subtext=subtext,
+            iconUrls=self.iconUrls,
+            actions=[
+                Action(
+                    "open",
+                    "Open",
+                    lambda args=run_args: runDetachedProcess(args),
+                ),
+                Action("copy", "Copy URI", lambda uri=note_uri: setClipboardText(uri)),
+            ],
+        )
